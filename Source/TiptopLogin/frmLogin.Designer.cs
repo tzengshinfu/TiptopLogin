074c10df39e73134d1e028f43312fd1fd8a58fa4
tzengshinfu <tzengshinfu@gmail.com>
2019-12-03 17:07:46 +0800

    First commit.

---
+++ Source/TiptopLogin/frmLogin.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace TiptopLogin {
+    partial class frmLogin {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.lblAccount = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblMode = new System.Windows.Forms.Label();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.rbxProd = new System.Windows.Forms.RadioButton();
+            this.rbxTest = new System.Windows.Forms.RadioButton();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtAccount = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.icoNotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.pictureBox1);
+            this.flowLayoutPanel1.Controls.Add(this.tableLayoutPanel1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(472, 246);
+            this.flowLayoutPanel1.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(471, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31.62393F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 68.37607F));
+            this.tableLayoutPanel1.Controls.Add(this.lblHost, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblAccount, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblPassword, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblPort, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lblMode, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.txtHost, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtAccount, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtPassword, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtPort, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnLogin, 1, 5);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 44);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.80198F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.87129F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(468, 202);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(3, 0);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(135, 15);
+            this.lblHost.TabIndex = 0;
+            this.lblHost.Text = "主機名稱(或地址)";
+            // 
+            // lblAccount
+            // 
+            this.lblAccount.AutoSize = true;
+            this.lblAccount.Location = new System.Drawing.Point(3, 33);
+            this.lblAccount.Name = "lblAccount";
+            this.lblAccount.Size = new System.Drawing.Size(87, 15);
+            this.lblAccount.TabIndex = 1;
+            this.lblAccount.Text = "使用者名稱";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(3, 66);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(39, 15);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "密碼";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(3, 99);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(39, 15);
+            this.lblPort.TabIndex = 3;
+            this.lblPort.Text = "Port";
+            // 
+            // lblMode
+            // 
+            this.lblMode.AutoSize = true;
+            this.lblMode.Location = new System.Drawing.Point(3, 139);
+            this.lblMode.Name = "lblMode";
+            this.lblMode.Size = new System.Drawing.Size(39, 15);
+            this.lblMode.TabIndex = 4;
+            this.lblMode.Text = "模式";
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.rbxProd);
+            this.flowLayoutPanel2.Controls.Add(this.rbxTest);
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(150, 142);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(295, 20);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // rbxProd
+            // 
+            this.rbxProd.AutoSize = true;
+            this.rbxProd.Checked = true;
+            this.rbxProd.Location = new System.Drawing.Point(3, 3);
+            this.rbxProd.Name = "rbxProd";
+            this.rbxProd.Size = new System.Drawing.Size(76, 19);
+            this.rbxProd.TabIndex = 3;
+            this.rbxProd.TabStop = true;
+            this.rbxProd.Text = "正式區";
+            this.rbxProd.UseVisualStyleBackColor = true;
+            // 
+            // rbxTest
+            // 
+            this.rbxTest.AutoSize = true;
+            this.rbxTest.Location = new System.Drawing.Point(85, 3);
+            this.rbxTest.Name = "rbxTest";
+            this.rbxTest.Size = new System.Drawing.Size(76, 19);
+            this.rbxTest.TabIndex = 4;
+            this.rbxTest.TabStop = true;
+            this.rbxTest.Text = "測試區";
+            this.rbxTest.UseVisualStyleBackColor = true;
+            // 
+            // txtHost
+            // 
+            this.txtHost.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtHost.Location = new System.Drawing.Point(150, 3);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(315, 25);
+            this.txtHost.TabIndex = 3;
+            this.txtHost.TabStop = false;
+            // 
+            // txtAccount
+            // 
+            this.txtAccount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtAccount.Location = new System.Drawing.Point(150, 36);
+            this.txtAccount.Name = "txtAccount";
+            this.txtAccount.Size = new System.Drawing.Size(315, 25);
+            this.txtAccount.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPassword.Location = new System.Drawing.Point(150, 69);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(315, 25);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPort.Location = new System.Drawing.Point(150, 102);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(315, 25);
+            this.txtPort.TabIndex = 9;
+            this.txtPort.TabStop = false;
+            this.txtPort.Text = "23";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogin.Location = new System.Drawing.Point(390, 168);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "連線";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // icoNotify
+            // 
+            this.icoNotify.Icon = ((System.Drawing.Icon)(resources.GetObject("icoNotify.Icon")));
+            this.icoNotify.Text = "Tiptop連線";
+            this.icoNotify.Visible = true;
+            this.icoNotify.BalloonTipClicked += new System.EventHandler(this.icoNotify_BalloonTipClicked);
+            this.icoNotify.Click += new System.EventHandler(this.icoNotify_Click);
+            this.icoNotify.DoubleClick += new System.EventHandler(this.icoNotify_DoubleClick);
+            // 
+            // frmLogin
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 246);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tiptop連線";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.Label lblAccount;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblMode;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.RadioButton rbxProd;
+        private System.Windows.Forms.RadioButton rbxTest;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtAccount;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.NotifyIcon icoNotify;
+    }
+}
---
+++ Source/TiptopLogin/frmLogin.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace TiptopLogin {
+    public partial class frmLogin : Form {
+        public frmLogin() {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e) {
+            try {
+                #region 檢查
+                if (txtHost.Text == "") {
+                    throw new Exception("[" + lblHost.Text + "]" + "要有值");
+                }
+
+                if (txtAccount.Text == "") {
+                    throw new Exception("[" + lblAccount.Text + "]" + "要有值");
+                }
+
+                if (txtPassword.Text == "") {
+                    throw new Exception("[" + lblPassword.Text + "]" + "要有值");
+                }
+
+                if (txtPort.Text == "") {
+                    throw new Exception("[" + lblPort.Text + "]" + "要有值");
+                }
+                #endregion
+
+                var mode = rbxProd.Checked ? "1" : "2";
+
+                StartDosCommand(@"C:\\PROGRAM FILES (X86)\\FOURJS\\GDC\\BIN\\GDC.EXE", " -M");
+
+                var connection = new Telnet(txtHost.Text, Convert.ToInt32(txtPort.Text));
+
+                if (connection.ResponseIncludes("login:")) {
+                    connection.SendKeys(txtAccount.Text);
+                }
+
+                if (connection.ResponseIncludes("Password:")) {
+                    connection.SendKeys(txtPassword.Text);
+                }
+
+                if (connection.ResponseIncludes("Please Select [1]topprod[2]toptest [*]Exit [1]:")) {
+                    connection.SendKeys(mode);
+                }
+                else {
+                    throw new Exception("無法登入,請確認帳密是否正確");
+                }
+
+                Hide();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 背景執行Dos指令(無視窗)
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="arguments"></param>
+        private void StartDosCommand(string fileName, string arguments) {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = fileName;
+            processStartInfo.Arguments = arguments;
+            processStartInfo.RedirectStandardOutput = true;
+            processStartInfo.RedirectStandardError = true;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.CreateNoWindow = true;
+
+            Process process = new Process();
+            process.StartInfo = processStartInfo;
+            process.EnableRaisingEvents = true;
+            process.Start();
+        }
+
+        private void icoNotify_BalloonTipClicked(object sender, EventArgs e) {
+            ShowForm();
+        }
+
+        private void ShowForm() {
+            Show();
+            Activate();
+            Focus();
+        }
+
+        private void icoNotify_DoubleClick(object sender, EventArgs e) {
+            ShowForm();
+        }
+
+        private void icoNotify_Click(object sender, EventArgs e) {
+            ShowForm();
+        }
+    }
+}
